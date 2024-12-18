9e6fb4bd733c365d81aa08ec4155e2357e33ce35
pro-soo <rhackdtn@naver.com>
2024-12-18 13:11:50 +0900

    컴포넌트 추가,  css 수정

--- src/pages/posts/new.tsx
+++ src/pages/posts/new.tsx
@@ -1,3 +1,13 @@
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import PostList from "../../components/PostList";
+
 export default function PostNew(){
-    return <h1>Post New Page</h1>;
+    return (
+        <>
+        <Header/>
+        <div>Post Form</div>
+        <Footer/>
+        </>
+    );
 }
