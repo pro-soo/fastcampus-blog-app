a0a97d055b871cabfb5a72cb05f05248fd92f8fc
pro-soo <rhackdtn@naver.com>
2024-12-23 14:22:09 +0900

    form 컴포넌트 작업

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom"; 
+
 export default function PostDetail() {
     return <>
     <div className="post__detail">
@@ -12,7 +14,9 @@ export default function PostDetail() {
             </div>
             <div className="post__utils-box">
                 <div className="post__delete">삭제</div>
-                <div className="post__edit">수정</div>
+                <div className="post__edit">
+                    <Link to={`/posts/edit/1`}>수정</Link>
+                </div>
             </div>
             <div className="post__text">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,16 +1,22 @@
+import {useState} from "react";
 import { Link } from "react-router-dom";
 
 interface PostListProps {
     hasNavigation? : boolean;
 }
 
+type TabType = "all" | "my";
+
 export default function PostList({hasNavigation = true}:PostListProps){
+    const [activeTab, setActiveTeb] = useState<TabType>("all");
     return (
         <>
         {hasNavigation && (
         <div className="post__navigation">
-            <div className="post__navigation--active">전체</div>
-            <div>나의 글</div>
+            <div role="presentation" onClick={() => setActiveTeb('all')} 
+            className={activeTab === 'all' ? 'post__navigation--active' : ""}>전체</div>
+            <div role="presentation" onClick={() => setActiveTeb('my')}
+            className={activeTab === 'my' ? 'post__navigation--active' : ""}>나의 글</div>
         </div>
         )}
         <div className="post__list">
