50cdd268ca68f73239ddf8de0251203b794cbce2
pro-soo <rhackdtn@naver.com>
2024-12-26 17:57:59 +0900

    다크모드 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
 import { app } from 'firebaseApp';
 import {getAuth, onAuthStateChanged} from "firebase/auth";
 import { ToastContainer } from 'react-toastify';
@@ -6,8 +6,10 @@ import "react-toastify/dist/ReactToastify.css";
 
 import Router from './components/Router';
 import Loader from 'components/Loader';
+import ThemeContext from 'context/ThemeContext';
 
 function App() {
+  const context = useContext(ThemeContext);
   const auth = getAuth(app);
   // auth를 체크하기 전에 (initialize 전)에는 loader를 띄워주는 용도
   const [init, setInit] = useState<boolean>(false);
@@ -26,10 +28,10 @@ function App() {
   }, [auth]);
   
   return (
-  <>
+  <div className={context.theme === 'light' ? 'white' : 'dark'}>
   <ToastContainer/>
   {init ? <Router isAuthenticated={isAuthenticated}/> : <Loader/>}
-  </>
+  </div>
   );
 }
 
