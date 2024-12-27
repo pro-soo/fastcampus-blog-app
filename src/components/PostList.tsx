c80f2119218915621b1acdf8327135ce09e04485
pro-soo <rhackdtn@naver.com>
2024-12-27 13:30:15 +0900

    댓글 기능 구현

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -12,6 +12,13 @@ interface PostListProps {
 
 type TabType = "all" | "my";
 
+export interface CommentsInterface {
+    content: string;
+    uid: string;
+    email: string;
+    createdAt: string;
+}
+
 export interface PostProps {
     id?: string;
     title: string;
@@ -21,7 +28,8 @@ export interface PostProps {
     createdAt: string;
     updatedAt: string;
     uid: string;
-    category: CategoryType;
+    category?: CategoryType;
+    comments?: CommentsInterface[];
 }
 
 
